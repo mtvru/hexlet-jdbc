5be512c01976a6365dc3c9074eea2ae2ee1e2684
maksimus <max.expertdev@gmail.com>
2026-01-19 13:47:38 +0600

    PreparedStatement

--- src/main/java/io/hexlet/Application.java
+++ src/main/java/io/hexlet/Application.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 public class Application {
@@ -14,11 +15,32 @@ public class Application {
                 statement.execute(sql);
             }
 
-            String sql2 = "INSERT INTO users (username, phone) VALUES ('hommy', '123456789')";
-            try (Statement statement2 = conn.createStatement()) {
-                statement2.executeUpdate(sql2);
-            }
+            String sql2 = "INSERT INTO users (username, phone) VALUES (?, ?)";
+            try (PreparedStatement preparedStatement = conn.prepareStatement(sql2, Statement.RETURN_GENERATED_KEYS)) {
+                preparedStatement.setString(1, "hommy");
+                preparedStatement.setString(2, "123456789");
+                preparedStatement.executeUpdate();
 
+                preparedStatement.setString(1, "whoopy");
+                preparedStatement.setString(2, "123456789");
+                preparedStatement.executeUpdate();
+
+                ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
+                if (generatedKeys.next()) {
+                    long id = generatedKeys.getLong(1);
+                    String sql21 = "DELETE FROM users WHERE id = ?";
+                    try (PreparedStatement preparedStatement2 = conn.prepareStatement(sql21)) {
+                        preparedStatement2.setLong(1, id);
+                        preparedStatement2.executeUpdate();
+                    }
+                } else {
+                    throw new SQLException("DB have not returned an id after saving the entity");
+                }
+
+                preparedStatement.setString(1, "largo");
+                preparedStatement.setString(2, "123456789");
+                preparedStatement.executeUpdate();
+            }
             String sql3 = "SELECT * FROM users";
             try (Statement statement3 = conn.createStatement()) {
                 // Здесь вы видите указатель на набор данных в памяти СУБД
